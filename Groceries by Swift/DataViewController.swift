20ce3c181ea341822bb4eaa52fb2305b0730cada
rameshts <AppsAppa@rameshs-mini.attlocal.net>
2014-10-28 18:22:33 -0500

    Adding Table View to pages

--- Groceries by Swift/DataViewController.swift
+++ Groceries by Swift/DataViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class DataViewController: UIViewController {
+class DataViewController: UITableViewController {
 
     @IBOutlet weak var dataLabel: UILabel!
     var dataObject: AnyObject?
+    
+    var groceries: [String] = ["1 gallon of regular milk", "Bud light 6 pack", "Whole wheat bread", "Dozen doughnuts"]
 
 
     override func viewDidLoad() {
@@ -26,13 +28,26 @@ class DataViewController: UIViewController {
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        // Need to figure out how to set this label later....
         if let obj: AnyObject = dataObject {
-            self.dataLabel!.text = obj.description
+            // self.dataLabel!.text = obj.description
         } else {
-            self.dataLabel!.text = ""
+            // self.dataLabel!.text = ""
         }
     }
-
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return groceries.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("tableCell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = groceries[indexPath.row]
+        return cell
+    }
 
 }
 
